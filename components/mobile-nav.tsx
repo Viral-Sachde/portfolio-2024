6dc5bcff18b8f52da5ec7dc2d2c08c1bc9d7a7e3
Viral Sachde <133246827+Viral-Sachde@users.noreply.github.com>
2024-12-21 15:12:29 +0530

    Update mobile-nav.tsx

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -10,7 +10,7 @@ const links = [
   { href: "/project", label: "Projects" },
   { href: "/articles", label: "Articles" },
   { href: "/contact", label: "Contact" },
-  { href: "/contact", label: "Resume" },
+  { href: "/https://drive.google.com/file/d/1sp2xqfnzmuITIwvKVatzsVrnPq5TECQJ/view?usp=drive_link", label: "Resume" },
 ];
 
 
